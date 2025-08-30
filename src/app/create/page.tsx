750b8b481f9c1220ea4c07f6b43f303da47b1bcf
marcus <changxuhong10@gmail.com>
2025-08-31 03:13:06 +1000

    fixing create and progress page size

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,10 +2,13 @@ import {Board} from "~/components/whiteboard/board";
 
 export default function CreatePage() {
   return (
-    <div>
+    <div className="flex flex-col h-screen w-screen">
       <h1 className="text-2xl font-bold mb-4">Create</h1>
-      <p>This is the Create page. Add your creation logic here.</p>
-        <Board/>
+      <main className="flex-1 w-full">
+        {/* Make Board take full available space */}
+        <Board />
+      </main>
+
     </div>
   );
 }
