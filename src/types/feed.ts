61043d477e93c60dae04d77ce3141470168bb8ba
marcus <changxuhong10@gmail.com>
2025-08-31 10:43:15 +1000

    buttons...

--- src/types/feed.ts
+++ src/types/feed.ts
@@ -6,7 +6,7 @@ export interface Post {
   image: string; // URL to the post image
   friend: string; // URL to friend's avatar
   name: string;   // Friend's name
-  description?: string; // Optional description or caption
+  description: string; // Optional description or caption
 
 }
 
